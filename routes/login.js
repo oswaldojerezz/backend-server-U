39a8d77c6d8484440c8aecef4a30d647b94232cb
Oswaldo Jerez <ojerez@urbe.edu.ve>
2019-07-06 17:07:49 -0600

    Renovation Token Aut.

--- routes/login.js
+++ routes/login.js
@@ -16,6 +16,24 @@ var CLIENT_ID = require('../config/config').CLIENT_ID;
 const { OAuth2Client } = require('google-auth-library');
 const client = new OAuth2Client(CLIENT_ID);
 
+var mdAutenticacion = require('../middlewares/autenticacion');
+
+// ==========================================
+// Autenticacion Google
+// ==========================================
+
+app.get('/renuevatoken', mdAutenticacion.verificaToken, (req, res => {
+
+    var token = jwt.sign({ usuario: req.usuario }, SEED, { expiresIn: 14000 }); // 4 horas
+
+    res.status(200).json({
+        ok: true,
+        token: token
+    });
+
+}));
+
+
 // ==========================================
 // Autenticacion Google
 // ==========================================
